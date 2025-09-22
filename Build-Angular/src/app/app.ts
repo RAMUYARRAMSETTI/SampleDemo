05ba1b2113e54bf00e2bc45b6e534ee5d31ba0bb
Y_Ramu <yarramsettiramu999@gmail.com>
2025-09-22 20:22:11 +0530

    Will check

--- Build-Angular/src/app/app.ts
+++ Build-Angular/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('SRC');
+  protected readonly title = signal('Build-Angular');
 }
